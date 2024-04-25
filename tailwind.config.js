0ee430dabef212f64d4ed7b76736aa0180569f8b
Matthieu <matthieu.delarue5@gmail.com>
2024-04-25 16:48:49 +0200

    🎉 Init angular & tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  plugins: [require("@tailwindcss/forms")],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        white: "#ffffff",
+        black: "#09090b",
+        gray: {
+          50: "#fafafa",
+          100: "#f4f4f5",
+          200: "#e4e4e7",
+          300: "#d4d4d8",
+          400: "#a1a1aa",
+          500: "#71717a",
+          600: "#52525b",
+          700: "#3f3f46",
+          800: "#27272a",
+          900: "#18181b",
+          950: "#09090b",
+        },
+        primary: {
+          50: "#f3f5ff",
+          100: "#e9edfe",
+          200: "#d5deff",
+          300: "#b4c0fe",
+          400: "#8997fc",
+          500: "#5963f9",
+          600: "#3636f1",
+          700: "#2825dc",
+          800: "#211eb9",
+          900: "#1d1b97",
+          950: "#121481",
+        },
+        secondary: {
+          50: "#fff5f1",
+          100: "#ffeae3",
+          200: "#fed7cd",
+          300: "#fdb6a4",
+          400: "#fb8a71",
+          500: "#f45b3f",
+          600: "#e1321d",
+          700: "#be2012",
+          800: "#9f1912",
+          900: "#881513",
+          950: "#4c0805",
+        },
+      },
+    },
+  },
+};
