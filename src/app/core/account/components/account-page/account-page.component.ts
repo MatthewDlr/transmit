f90b393324c75397a08972d4f62f6ecf40d0c6b6
Matthieu <matthieu.delarue5@gmail.com>
2024-04-30 15:40:32 +0200

    ✨ Add personal info in the account page

--- src/app/core/account/components/account-page/account-page.component.ts
+++ src/app/core/account/components/account-page/account-page.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { LogoutFormComponent } from '../../../../auth/components/logout-form/logout-form.component';
+import { Component, effect } from "@angular/core";
+import { LogoutFormComponent } from "../../../../auth/components/logout-form/logout-form.component";
+import { UserProfileService } from "../../../../shared/services/user-profile/user-profile.service";
+import { UserProfile } from "../../../../shared/types/Profile.type";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-account-page',
+  selector: "app-account-page",
   standalone: true,
-  imports: [LogoutFormComponent],
-  templateUrl: './account-page.component.html',
-  styleUrl: './account-page.component.css'
+  imports: [LogoutFormComponent, FormsModule],
+  templateUrl: "./account-page.component.html",
+  styleUrl: "./account-page.component.css",
 })
 export class AccountPageComponent {
+  user!: UserProfile;
+  updateStatus: string = "OK";
 
+  constructor(private userService: UserProfileService) {
+    effect(() => {
+      const user = this.userService.userProfile();
+      if (user !== null) this.user = user;
+    });
+  }
+
+  async saveUserProfile() {
+    this.updateStatus = "Loading";
+    this.updateStatus = await this.userService.update(this.user);
+  }
 }
