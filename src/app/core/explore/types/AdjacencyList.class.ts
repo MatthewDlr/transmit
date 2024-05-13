e4b52f5cb8225aa023b03efca9fd75efca91e884
Matthieu <matthieu.delarue5@gmail.com>
2024-05-13 11:16:44 +0200

    🔊 Add logs to graph types

--- src/app/core/explore/types/AdjacencyList.class.ts
+++ src/app/core/explore/types/AdjacencyList.class.ts
@@ -9,6 +9,7 @@ export class AdjacencyNodeList {
     this.nodes = new Map();
   }
 
+  // Add new user in the keys
   public addNode(id: string, depth: number, radius = 0) {
     this.nodes.forEach((_, user) => {
       if (user.id === id) return;
@@ -16,6 +17,7 @@ export class AdjacencyNodeList {
     this.nodes.set({ id, depth: depth, radius: radius }, new Set());
   }
 
+  // Add a new friend (target) for a given person (source)
   public addLinks(source: string, targets: string[], depth: number) {
     const sourceNode: GraphNode | undefined = this.getNode(source);
 
@@ -30,6 +32,7 @@ export class AdjacencyNodeList {
     }
   }
 
+  // Register a new follower for a given user ID
   private incrementRadius(nodeID: string, depth: number) {
     const node = this.getNode(nodeID);
     if (node) {
@@ -39,14 +42,17 @@ export class AdjacencyNodeList {
     }
   }
 
+  // Get a user (key) with his ID
   private getNode(id: string): GraphNode | undefined {
     return Array.from(this.nodes.keys()).find((user) => user.id === id);
   }
 
+  // Get all user of the list
   public getNodes(): GraphNode[] {
     return Array.from(this.nodes.keys());
   }
 
+  // Get of friends of all user in the form of source -> tagret
   public getLinks(): SimulationLinkDatum<GraphNode>[] {
     const links: SimulationLinkDatum<GraphNode>[] = [];
     this.nodes.forEach((values, node) => {
--- src/app/core/explore/types/GraphNode.interface copy.ts
+++ src/app/core/explore/types/GraphNode.interface copy.ts
@@ -2,5 +2,5 @@ import { SimulationLinkDatum } from "d3";
 import { GraphNode } from "./GraphNode.interface";
 
 export interface GraphLink extends SimulationLinkDatum<GraphNode> {
-  id: number;
+  id: number; // ID of the user profile
 }
