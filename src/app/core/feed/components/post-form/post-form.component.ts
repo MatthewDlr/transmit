2decebbd6dc3e1fcd78aaa4f1427e89a2ca23ebb
Ghislain Demaël <demaelghislain@gmail.com>
2024-05-14 23:45:10 +0200

    Post form reset on succesful posting

--- src/app/core/feed/components/post-form/post-form.component.ts
+++ src/app/core/feed/components/post-form/post-form.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ImageUploadComponent} from "../image-upload/image-upload.component";
-import {UserProfileService} from "../../../../shared/services/user-profile/user-profile.service";
-import {Post} from "../../../../shared/types/Post.type";
+
 import {PostPublishingService} from "../../../../shared/services/post-publishing/post-publishing.service";
 
 @Component({
@@ -13,22 +12,37 @@ import {PostPublishingService} from "../../../../shared/services/post-publishing
   templateUrl: './post-form.component.html',
   styleUrl: './post-form.component.css'
 })
-export class PostFormComponent {
+export class PostFormComponent implements OnInit {
 
   selectedFile: File | null = null;
 
+  @ViewChild('imageUpload')
+  imageUpload!: ImageUploadComponent;
+
   constructor(private publishingService: PostPublishingService) { }
+
+  ngOnInit(): void {
+
+  }
 
   onFileSelected(file: File): void {
     this.selectedFile = file;
   }
 
   postPost(value: string) {
-    if(value.length <= 10){
+    if(value.length <= 5){
       console.log("Not enough content");
       return;
     }
-    this.publishingService.publishPost(value, this.selectedFile).then(r => console.log("Post successful"));
+    this.publishingService.publishPost(value, this.selectedFile).then(r => {
+      console.log("Post successful");
+      this.selectedFile = null;
+      const textInput = document.querySelector('#textInput') as HTMLTextAreaElement;
+      if(textInput){
+        textInput.value = '';
+      }
+      this.imageUpload.reset();
+    });
   }
 
 }
