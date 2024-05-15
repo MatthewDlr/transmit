db133930c052df805a797b283c135c43583920a7
Ghislain Demaël <demaelghislain@gmail.com>
2024-05-16 00:49:44 +0200

    Message on post success or error, as well as error if text too short

--- src/app/core/feed/components/post-form/post-form.component.ts
+++ src/app/core/feed/components/post-form/post-form.component.ts
@@ -2,12 +2,19 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {ImageUploadComponent} from "../image-upload/image-upload.component";
 
 import {PostPublishingService} from "../../../../shared/services/post-publishing/post-publishing.service";
+import {NgClass, NgIf} from "@angular/common";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faCheckCircle} from "@fortawesome/free-solid-svg-icons/faCheckCircle";
+import {faTimesCircle} from "@fortawesome/free-solid-svg-icons/faTimesCircle";
 
 @Component({
   selector: 'app-post-form',
   standalone: true,
   imports: [
-    ImageUploadComponent
+    ImageUploadComponent,
+    NgIf,
+    NgClass,
+    FaIconComponent
   ],
   templateUrl: './post-form.component.html',
   styleUrl: './post-form.component.css'
@@ -18,6 +25,9 @@ export class PostFormComponent implements OnInit {
 
   @ViewChild('imageUpload')
   imageUpload!: ImageUploadComponent;
+  message: string | null = null;
+  isPopupVisible: boolean = false;
+  isError: boolean = false;
 
   constructor(private publishingService: PostPublishingService) { }
 
@@ -31,18 +41,48 @@ export class PostFormComponent implements OnInit {
 
   postPost(value: string) {
     if(value.length <= 5){
-      console.log("Not enough content");
+      this.isError = true;
+      this.message = "Please add more content.";
+      this.isPopupVisible = true;
+      setTimeout(() => {
+        this.isPopupVisible = false;
+        setTimeout(() => {
+          this.message = null;
+          this.isError = false;
+        }, 1000);
+      }, 4000);
       return;
     }
-    this.publishingService.publishPost(value, this.selectedFile).then(r => {
-      console.log("Post successful");
+    this.publishingService.publishPost(value, this.selectedFile).then(() => {
+      this.message = "Post successful";
+      this.isPopupVisible = true;
+      setTimeout(() => {
+        this.isPopupVisible = false;
+        setTimeout(() => {
+          this.message = null;
+        }, 1000);
+      }, 4000);
       this.selectedFile = null;
       const textInput = document.querySelector('#textInput') as HTMLTextAreaElement;
       if(textInput){
         textInput.value = '';
       }
       this.imageUpload.reset();
+    }).catch(()=> {
+      this.message = "An error occurred. Please try again.";
+      this.isError = true;
+      this.isPopupVisible = true;
+      setTimeout(() => {
+        this.isPopupVisible = false;
+        setTimeout(() => {
+          this.message = null;
+          this.isError = false;
+        }, 1000);
+      }, 4000);
+
     });
   }
 
+  protected readonly faCheckCircle = faCheckCircle;
+  protected readonly faTimesCircle = faTimesCircle;
 }
