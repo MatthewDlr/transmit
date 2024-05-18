4becf5dbb24ccf779e7fdaa1b7bf63e4b469f1bf
Matthieu <matthieu.delarue5@gmail.com>
2024-05-18 17:07:15 +0200

    📝 Add doc to recommendation service

--- src/app/core/explore/services/suggestions/suggestions.service.ts
+++ src/app/core/explore/services/suggestions/suggestions.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, WritableSignal, signal } from "@angular/core";
+import { Injectable, WritableSignal, effect, signal } from "@angular/core";
 import { UserProfile } from "../../../../shared/types/Profile.type";
-import {FoafService} from "../foaf/foaf.service";
+import { FoafService } from "../foaf/foaf.service";
+import { UserProfileService } from "../../../../shared/services/user-profile/user-profile.service";
 
 @Injectable({
   providedIn: "root",
@@ -8,30 +9,48 @@ import {FoafService} from "../foaf/foaf.service";
 export class SuggestionsService {
   users: WritableSignal<Set<UserProfile>> = signal(new Set());
 
-  constructor(private foafService: FoafService) { }
+  constructor(private foafService: FoafService, private userService: UserProfileService) {
+    const currentUser = this.userService.userProfile();
+
+    // This function is run whenever the isLoading signal change value
+    effect(() => {
+      const isLoading: boolean = this.foafService.isLoading();
+      if (isLoading) {
+        // Not all users are fetched yet, we do nothing
+        return;
+      } else {
+        // All users will be fully fetched now
+        this.addSuggestion(currentUser!);
+      }
+    });
+  }
 
   public async addSuggestion(user: UserProfile) {
     try {
       // Fetch friends of the given user
       await this.foafService.fetch(this.foafService.DEFAULT_MAX_DEPTH);
-      const userNode = this.foafService.getNodes().find(node => node.id === user.id);
+      const userNode = this.foafService.getNodes().find((node) => node.id === user.id);
 
       if (userNode) {
-        const friends = this.foafService.getLinks()
-          .filter(link => link.source === userNode.id)
-          .map(link => this.foafService.getNodes().find(node => node.id === link.target));
+        const friends = this.foafService
+          .getLinks()
+          .filter((link) => link.source === userNode.id)
+          .map((link) => this.foafService.getNodes().find((node) => node.id === link.target));
 
-        friends.forEach(friend => {
+        friends.forEach((friend) => {
           if (friend) {
             const friendProfile: UserProfile = {
-              last_name: "", updated_at: "",
+              last_name: "",
+              updated_at: "",
               id: friend.id,
-              name: ""
+              name: "",
             };
             this.users().add(friendProfile);
           }
         });
       }
+
+      console.log(this.users);
     } catch (error) {
       console.error(`Error fetching friends for user ${user.id}:`, error);
     }
