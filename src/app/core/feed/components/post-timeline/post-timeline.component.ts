325064cf65d9c1cd8022a82306b8749f9aad7914
Ghislain Demaël <demaelghislain@gmail.com>
2024-05-19 16:13:23 +0200

    Realtime post updates for post timeline component

--- src/app/core/feed/components/post-timeline/post-timeline.component.ts
+++ src/app/core/feed/components/post-timeline/post-timeline.component.ts
@@ -5,6 +5,7 @@ import { Post } from "../../../../shared/types/Post.type";
 import {NgForOf, NgIf, NgIfContext} from "@angular/common";
 import {User} from "@supabase/supabase-js";
 import {PostSuggestionService} from "../../services/post-suggestion/post-suggestion.service";
+import {SupabaseService} from "../../../../shared/services/supabase/supabase.service";
 
 
 @Component({
@@ -16,11 +17,29 @@ import {PostSuggestionService} from "../../services/post-suggestion/post-suggest
 })
 
 export class PostTimelineComponent implements OnInit {
-  posts: Post[] = [];
-  suggestionPosts: Post[] = [];
-  emptyPostList: TemplateRef<NgIfContext<boolean>> | undefined;
 
-  constructor(private postService: PostListService, private postSuggestionService: PostSuggestionService) { }
+  private supabase = this.supabaseService.client;
+  posts: Post[] = [];
+
+  constructor(private postService: PostListService, private supabaseService: SupabaseService) {
+    this.supabase.channel('posts')
+      .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'posts' }, this.handleInserts)
+      .subscribe();
+  }
+
+  handleInserts = async (payload: any) => {
+    console.log('New post !');
+    //console.log(payload);
+    const formattedName = await this.formatName(payload.new.created_by);
+    const newPost: Post = {
+      id: payload.new.id,
+      content: payload.new.content,
+      timestamp: new Date(payload.new.created_at),
+      author: formattedName,
+      authorNumber: payload.new.created_by,
+    };
+    this.posts.unshift(newPost);
+  }
 
   ngOnInit(): void {
     this.fetchPosts().then(r => console.log("OK"));
@@ -33,6 +52,27 @@ export class PostTimelineComponent implements OnInit {
     } catch (error) {
       console.error('Error fetching posts:', error);
     }
+  }
+
+  private async formatName(user_id: string): Promise<string> {
+    const { data, error } = await this.supabase
+      .from("profiles")
+      .select("name, last_name")
+      .eq("id", user_id)
+      .limit(1);
+
+    if (error) {
+      console.error(error);
+      return "";
+    }
+
+    if (data.length === 0) {
+      return user_id.substring(0, 10);
+    }
+    const name = data[0].name;
+    const lastName = data[0].last_name;
+
+    return `${name} ${lastName}`;
   }
 
   async fetchRecommendedPosts(): Promise<void> {
